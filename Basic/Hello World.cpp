646127a04df9c06053e0a985940a46f6a1722b13
Imran Sarkar <imran.sarkar@softbdltd.com>
2022-06-13 16:55:40 +0600

    ignore all .exe files

--- Basic/Hello World.cpp
+++ Basic/Hello World.cpp
@@ -4,8 +4,6 @@ int main()
 {
 
     std::cout << "Hello World, this is first C++ programming output." << std::endl;
-    std::cout << "this is second line" << std::endl;
-    std::cout << "this is third line" << std::endl;
     getchar();
     return 0;
 }
