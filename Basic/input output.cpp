df9e37fdbfbd2efe2ebcccb18625e0ef382d6288
Imran Sarkar <imran.sarkar@softbdltd.com>
2022-06-13 17:26:21 +0600

    add input output file

---
+++ Basic/input output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+int main()
+{
+    std::string s;
+    std::cout << "Enter your name : ";
+    std::cin >> s;
+    std::cout << "Your name is " << s << std::endl;
+
+    system("pause");
+    return 0;
+}
