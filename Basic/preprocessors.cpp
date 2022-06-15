21cafe4613c63138b17be6d5e5c53a2f8374b06f
Imran Sarkar <imran.sarkar@softbdltd.com>
2022-06-15 16:42:08 +0600

    no changes

--- Basic/preprocessors.cpp
+++ Basic/preprocessors.cpp
@@ -7,7 +7,10 @@ void printZeroToLimit()
 {
     for (int i = 0; i < LIMITE; i++)
     {
-        std::cout << "I = " << i << std::endl;
+        std::cout << "i = " << i << " ";
+
+        if (!(i % 5))
+            std::cout << std::endl;
     }
 }
 
@@ -16,10 +19,32 @@ void printArea()
     std::cout << "Area of 10 and 5 = " << AREA(10, 5) << std::endl;
 }
 
+class Mango
+{
+public:
+    Mango()
+    {
+        std::cout << "Mongo's constructor is called." << std::endl;
+    }
+    void get()
+    {
+        std::cout << "I send 30kg mango to you." << std::endl;
+    }
+    void put(int n)
+    {
+        std::cout << "I have received " << n << "kg mango from you " << std::endl;
+    }
+};
+
 int main()
 {
-    // printZeroToLimit();
+    printZeroToLimit();
     printArea();
+    std::cout << "hello" << std::endl;
+
+    Mango m;
+    m.get();
+    m.put(1000);
 
     system("pause");
     return 0;
