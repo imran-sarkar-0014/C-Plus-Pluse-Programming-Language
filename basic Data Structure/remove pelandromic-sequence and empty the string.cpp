850de139290285de98ee4278d81cfc42f7c6b205
Imran Sarkar <imran.sarkar@softbdltd.com>
2022-06-22 12:39:37 +0600

    no change.

--- basic Data Structure/remove pelandromic-sequence and empty the string.cpp
+++ basic Data Structure/remove pelandromic-sequence and empty the string.cpp
@@ -5,12 +5,23 @@ struct point
 {
     int start;
     int end;
+    point(int _start, int _end)
+    {
+        this->start = _start;
+        this->end = _end;
+    }
 };
 
 point max_pelandrom(std::string s)
 {
+    return point(1, 20);
 }
 
 int main()
 {
+
+    std::cout << "Hello World!" << std::endl;
+
+    system("pause");
+    return 0;
 }
